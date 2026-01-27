b0fc3c1f355de7e0e75a9c4e84009a22eecfe145
AbhinavKumarRajput <abhinav.kumar.rajput@inventia.in>
2026-01-27 13:56:39 +0530

    Update EmailHelper.cs

--- Helper/EmailHelper.cs
+++ Helper/EmailHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using System.Net.Mail;
 
@@ -7,27 +8,39 @@ namespace CRBS.Helper
     {
         public static bool Send(string to, string subject, string msg)
         {
-            MailMessage message = new MailMessage();
-            SmtpClient smtpClient = new SmtpClient();
-            message.From = new MailAddress("manusingh64431@gmail.com");
-            message.To.Add(to);
-            message.Subject = subject;
-            message.IsBodyHtml = true;
-            message.Body = msg;
-
-            smtpClient.Port = 587;
-            smtpClient.Host = "smtp.gmail.com";
-            smtpClient.EnableSsl = true;
-            smtpClient.UseDefaultCredentials = false;
-            smtpClient.Credentials = new NetworkCredential("manusingh64431@gmail.com", "dhkw sjae abaq ypis");
-            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
             try
             {
+                // Hardcoded email + SMTP key
+                string fromEmail = "manusingh64431@gmail.com";
+                string password = "xsmtpsib-a06dc43a4e5f22d973d5fb5e1e11050a42917c5be3185a6f6e9e7413f190ea6f-klFbeRnCKfy5OYBc";
+                string host = "smtp-relay.brevo.com";
+                int port = 587;
+
+                using MailMessage message = new MailMessage
+                {
+                    From = new MailAddress(fromEmail, "CRBS Team"),
+                    Subject = subject,
+                    Body = msg,
+                    IsBodyHtml = true
+                };
+
+                message.To.Add(to);
+
+                using SmtpClient smtpClient = new SmtpClient(host, port)
+                {
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromEmail, password),
+                    DeliveryMethod = SmtpDeliveryMethod.Network
+                };
+
                 smtpClient.Send(message);
+                Console.WriteLine("Email sent to " + to);
                 return true;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                Console.WriteLine("SMTP ERROR: " + ex.Message);
                 return false;
             }
         }
